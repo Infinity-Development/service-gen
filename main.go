7bcc510f198721bf4a37a2b1888244d3e36acae8
Your Name <you@example.com>
2022-12-12 17:18:03 +0000

    add target gen

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// Defines a template which is any FILENAME.yaml where FILENAME != _meta
 type TemplateYaml struct {
 	Command     string `yaml:"cmd" validate:"required"`         // ExecStart in systemd
 	Directory   string `yaml:"dir" validate:"required"`         // WorkingDirectory in systemd
@@ -20,8 +21,22 @@ type TemplateYaml struct {
 	After       string `yaml:"after" validate:"required"`       // After in systemd
 }
 
+// Defines metadata which is _meta.yaml
+type MetaYAML struct {
+	Targets []MetaTarget `yaml:"targets" validate:"required"` // List of targets to generate
+}
+
+// Defines a target in _meta.yaml:targets
+type MetaTarget struct {
+	Name        string `yaml:"name" validate:"required"`        // Name of target file
+	Description string `yaml:"description" validate:"required"` // Directory to place target file
+}
+
 //go:embed service.tmpl
 var serviceTemplate string
+
+//go:embed target.tmpl
+var targetTemplate string
 
 func main() {
 	if len(os.Args) < 2 {
@@ -46,7 +61,7 @@ func main() {
 
 		for _, file := range dir {
 			if !file.IsDir() {
-				// Generate service file
+				// Generate service file by setting the argument and calling main
 				fmt.Println("Generating service for " + file.Name())
 				os.Args[1] = dirName + "/" + file.Name()
 
@@ -64,6 +79,61 @@ func main() {
 
 	if err != nil {
 		panic(err)
+	}
+
+	// Handle _meta.yaml
+	if strings.HasSuffix(inpFile, "_meta.yaml") {
+		fmt.Println("Creating metadata for services")
+
+		var meta MetaYAML
+
+		err = yaml.Unmarshal(inp, &meta)
+
+		if err != nil {
+			panic(err)
+		}
+
+		// Validate input file
+		validator := validator.New()
+
+		err = validator.Struct(meta)
+
+		if err != nil {
+			panic(err)
+		}
+
+		// Generate target files
+		for _, target := range meta.Targets {
+			var targetTemplate = template.Must(template.New("target").Parse(targetTemplate))
+
+			// Output file is removal of suffix and addition of .target
+			outFile := target.Name + ".target"
+
+			if os.Getenv("OUTPUT_DIR") != "" {
+				outFile = os.Getenv("OUTPUT_DIR") + "/" + outFile
+			}
+
+			// Create output file
+			out, err := os.Create(outFile)
+
+			if err != nil {
+				panic(err)
+			}
+
+			err = targetTemplate.Execute(out, target)
+
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println("Generated " + outFile)
+
+			err = out.Close()
+
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 
 	// Parse input file
